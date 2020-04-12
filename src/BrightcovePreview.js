c772b280f4b2fbc31b4bfd9ada91e99060112139
jameschristou <james.christou@gmail.com>
2020-04-13 08:57:22 +1000

    Included code for brightcove video preview

--- src/BrightcovePreview.js
+++ src/BrightcovePreview.js
@@ -1,8 +1,32 @@
+import React, {useEffect} from 'react';
+
 const BrightcovePreview = ({props}) => {
   const {attributes, className, setAttributes} = props;
 
+  const playerId = "default";
+  const accountId = "674523943001";
+  const videoId = "6146437284001";
+
+  useEffect(() => {
+    const script = document.createElement('script');
+  
+    script.src = `https://players.brightcove.net/${accountId}/${playerId}_default/index.js`;
+    script.async = true;
+  
+    document.body.appendChild(script);
+  }, []);
+
   return (
-    <div>Brightcove Preview</div>
+    <div className="brightcove__video-wrapper">
+      <video 
+          data-video-id={`${videoId}`}
+          data-account={`${accountId}`}
+          data-player={`${playerId}`}
+          data-embed="default"
+          className="video-js inline-video"
+          controls>
+      </video>
+    </div>
   );
 }
 
